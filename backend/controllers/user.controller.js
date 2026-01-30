b72806a4e2d3785c9a511abef3eb5e61ab29bbbc
Shavez khan <shavezkhan75056291@gmail.com>
2026-01-30 15:45:54 +0530

    created user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import uploadOnCloudinary from "../config/cloudinary.js";
 import User from "../models/user.model.js";
 
 export const getCurrentUser = async (req, res) => {
@@ -10,5 +11,25 @@ export const getCurrentUser = async (req, res) => {
     return res.status(200).json(user);
   } catch (error) {
     return res.status(500).json({ message: `cuurent user ${error}` });
+  }
+};
+
+export const editProfile = async (req, res) => {
+  try {
+    let { name } = req.body;
+    let image;
+    if (req.file) {
+      image = await uploadOnCloudinary(req.file.path);
+      let user = await User.findByIdAndUpdate(req.userId, {
+        name,
+        image,
+      });
+      if (!user) {
+        return res.status(400).json({ messsage: "user not found" });
+      }
+      return res.status(200).json(user);
+    }
+  } catch (error) {
+    return res.status(500).json({ message: `profile error ${error}` });
   }
 };
